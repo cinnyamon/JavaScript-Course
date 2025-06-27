c1e79228ee42aaa30708fa6bb0073fb13c9d3d8f
nippah <band.maid@disroot.org>
2025-06-27 10:42:11 +0300

    Fix entire broken website by retarded programming youtuber and add select functionality

---
+++ data/cart.js
@@ -0,0 +1,64 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+export function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function updateCartOnLoad() {
+  const localStorageCartItems = JSON.parse(localStorage.getItem('cart'))
+
+  let totalQuantity = 0;
+  localStorageCartItems.forEach(element => {
+    totalQuantity += element.quantity
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML = totalQuantity
+}
+
+export function addToCart(productId) {
+  let matchingItem;
+  const quantityPicked = Number(document.querySelector(`.js-quantity-picker-${productId}`).value)
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += quantityPicked;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: quantityPicked
+    });
+  }
+
+  
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
